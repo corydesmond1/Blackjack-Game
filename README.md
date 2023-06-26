7eb96ce5ef31d30b88880a073115bcc6007e07ef
Cory <corydesmond14@gmail.com>
2023-06-25 23:38:00 -0400

    completed game

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 This is a simple implementation of the popular casino card game Blackjack. The game is played between a player and a dealer, and the goal is to get a hand value as close to 21 as possible without exceeding it. The player with a hand value closest to 21 without busting wins the game.
 
 ### Features
-Deck creation: The deck of cards is created at the beginning of the game.
+Deck creation: The deck of cards is created at the beginning of the game when the window loads.
 Deck shuffling: The deck is shuffled using the Fisher-Yates algorithm to ensure randomness.
-Initial hand: The player and the dealer are each dealt two cards to start the game.
+Initial hand: The player is dealt 2 cards and the dealers hand has already been played and awaiting your turn to end.
 Hit: The player can request an additional card (hit) to increase their hand value.
-Stay: The player can choose to end their turn and let the dealer play.
-Dealer's turn: The dealer will continue to draw cards until their hand value reaches 17 or more.
+Stay: The player can choose to end their turn and result the hands of both player and dealer.
+Dealer's turn: The dealer, on load, has already drawn cards until their hand value reaches 17 or more.
 Ace card handling: The value of the Ace card dynamically changes between 1 and 11 based on the hand's total.
 Win determination: The game determines the winner based on the hand values of the player and the dealer.
 User interface: The game provides a simple user interface with card images and messages indicating the game's progress and outcome.
@@ -27,15 +27,10 @@ When the game loads, you will see the initial state of the game with the dealer'
 Your current hand value is displayed on the screen. You can also see the value of the dealer's face-up card.
 Click the "Hit" button to draw an additional card from the deck.
 Keep hitting until you are satisfied with your hand or until you bust (hand value exceeds 21).
-Click the "Stay" button to end your turn and let the dealer play.
+Click the "Stay" button to end your turn and let the game end.
 The dealer will draw cards until their hand value reaches 17 or more.
 The game will determine the winner based on the hand values of the player and the dealer.
 The outcome of the game will be displayed on the screen.
-You can click the "Restart" button to start a new game.
-### License
-This project is licensed under the MIT License. You are free to modify and use the code as per the license terms.
 
 ### Acknowledgements
-The card images used in the game are from https://example.com.
 The shuffle algorithm used is based on the Fisher-Yates algorithm.
-Feel free to customize this README file based on your specific project, adding more details or sections as needed.
--- blackjack.js
+++ blackjack.js
@@ -8,184 +8,170 @@ let deck = [];
 let playerAce = 0;
 let dealerAce = 0;
 
+window.onload = function () {
+  createDeck();
+  shuffleDeck();
+  initGame();
+  document.getElementById("playerhand").innerText = "Player -" + playerhandSum;
+};
 
+function createDeck() {
+  values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  types = ["C", "H", "D", "S"];
+  deck = [];
 
-
-
-window.onload = function() {
-    createDeck();
-    shuffleDeck();
-    initGame();
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push(values[j] + "-" + types[i]);
+    }
+  }
+  console.log(deck);
 }
 
-
-  function createDeck() {
-    values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    types = ['C', 'H', 'D', 'S']
-    deck = [];
-    
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + '-' + types[i]);
-        }
-    }
-    console.log(deck);
+function shuffleDeck() {
+  //fischer yates method
+  for (let i = 0; i < deck.length; i++) {
+    let j = Math.floor(Math.random() * deck.length);
+    //swap elements
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+  // console.log(deck);
 }
 
-  function shuffleDeck() {
-    //fischer yates method 
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length);
-        //swap elements
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-    // console.log(deck);
-}
+function initGame() {
+  hide = deck.pop();
+  dealerhandSum += getValue(hide);
+  dealerAce += getHandValue(hide);
 
-  function initGame() {
-    hide = deck.pop();
-    dealerhandSum += getValue(hide);
-    dealerAce += getHandValue(hide);
-    
-    while (dealerhandSum < 17) {
-        let card = deck.pop();
-        let cardImg = document.createElement('img');
-        cardImg.src = './blackjack-cards/' + card + '.png';
-        dealerhandSum += getValue(card);
-        dealerAce += getHandValue(card);
-        document.getElementById('dealer-card').append(cardImg);
+  while (dealerhandSum < 17) {
+    let card = deck.pop();
+    let cardImg = document.createElement("img");
+    cardImg.src = "./blackjack-cards/" + card + ".png";
+    dealerhandSum += getValue(card);
+    dealerAce += getHandValue(card);
+    document.getElementById("dealer-card").append(cardImg);
+  }
 
+  console.log(hide);
+  console.log("Dealer:", dealerhandSum);
 
-    }
-    
-      console.log(hide);
-      console.log('Dealer:', dealerhandSum);
-    
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement('img');
-        let card = deck.pop();
-        cardImg.src = './blackjack-cards/' + card + '.png';
-        playerHand.push(card);
-        playerhandSum += getValue(card);
-        playerAce += getHandValue(card);
-        document.getElementById('player-card').append(cardImg);
-    }
-    console.log('Player:', playerhandSum);
-    
-    
-}
-
-
-  function getValue(card) {
-    
-        const cardValue = card.split('-')[0];
-        const cardValues = {
-            A: 1,
-            K: 10,
-            Q: 10,
-            J: 10,
-        };
-        return cardValues[cardValue] || parseInt(cardValue);
-    }
-      
-
-
-  function getHandValue(hand) {
-    let value = 0;
-    let aceCount = 0;
-    
-      for (let i = 0; i < hand.length; i++) {
-        const card = hand[i].split('-')[0];
-        if(card === 'A') {
-            aceCount++;
-        } else {
-          value += getValue(card);
-        }
-    }
-      while (aceCount > 0) {
-        if (value + 11 > 21) {
-          value += 1;
-        } else {
-          value += 11;
-        }
-        aceCount--;
-    }
-      return value;
-   
-}
-// try later
-// 
-
-
-
-
-
-  const hit = document.getElementById('hit');
-  const stay = document.getElementById('stay');
-
-hit.addEventListener('click', canHit);
-stay.addEventListener('click', canStay);
-
-
-
-  function canStay() {
-    
-    while (dealerhandSum < 17) {
-        const card = deck.pop();
-        dealerHand.push(card);
-        dealerhandSum += getValue(card);
-        dealerAce += getHandValue(dealerHand);
-        let message = '';
-    }
-     if (playerhandSum > 21){
-       message = 'Player Bust!, Dealer Wins!';
-      console.log('Player Bust!, Dealer Wins!')
-       
-    } else if (playerhandSum > dealerhandSum || dealerhandSum > 21) {
-        message = 'Player Wins!';
-        console.log('Player Wins!');
-      
-    } else if (playerhandSum > 21 && dealerhandSum <= 21) {
-       message = 'Dealer Wins!';
-      console.log('Dealer Wins!');
-     
-    } else if (playerhandSum > 21 && dealerhandSum > 21) {
-       
-       message = 'No Winner!';
-      console.log('No Winner!')
-    
-    } else if (playerhandSum < dealerhandSum) {
-        console.log('Dealer Wins!');
-       message = 'Dealer Wins!';
-    } else {
-        console.log('House Rules, Dealer Wins!');
-       message = 'Dealer Wins!';
-    }
-    document.getElementById('winmsg').innerText = message;
-    return;
-}
-
-  function canHit() {
-      let sumMsg = '';
-    const card = deck.pop();
+  for (let i = 0; i < 2; i++) {
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./blackjack-cards/" + card + ".png";
     playerHand.push(card);
     playerhandSum += getValue(card);
-    playerAce += getHandValue(playerHand);
-      if (playerhandSum > 21) {
-        console.log('Player Bust!, Dealer Wins!');
-        return;
-  }   else {
-        console.log('Player drew a card: ', card);
-        console.log('Player hand sum:', playerhandSum);
-        console.log('Player ace count', playerAce);
+    playerAce += getHandValue(card);
+    document.getElementById("player-card").append(cardImg);
+  }
+  console.log("Player:", playerhandSum);
+}
+
+function getValue(card) {
+  const cardValue = card.split("-")[0];
+  const cardValues = {
+    A: 11,
+    K: 10,
+    Q: 10,
+    J: 10,
+  };
+  return cardValues[cardValue] || parseInt(cardValue);
+}
+
+function getHandValue(hand) {
+  let value = 0;
+  let aceCount = 0;
+
+  for (let i = 0; i < hand.length; i++) {
+    const card = hand[i].split("-")[0];
+    if ((card, card[0] === "A")) {
+      aceCount++;
+    } else {
+      value += getValue(card);
     }
-      let cardImg = document.createElement('img');
-      cardImg.src = './blackjack-cards/' + card + '.png';
-      document.getElementById('player-card').append(cardImg);
-      return playerhandSum <= 21;
-    
-      document.queryselector('player-hand').innerText = sumMsg;
-      
+  }
+  while (aceCount > 0) {
+    if (value + 11 > 21) {
+      value += 1;
+    } else {
+      value += 11;
+    }
+    aceCount--;
+  }
+  return value;
+}
+// try later
+//
+
+const hit = document.getElementById("hit");
+const stay = document.getElementById("stay");
+
+hit.addEventListener("click", canHit);
+stay.addEventListener("click", canStay);
+
+function canStay() {
+  showDealerCard();
+  let message = "";
+  let sumMsg = "";
+  while (dealerhandSum < 17) {
+    const card = deck.pop();
+    dealerHand.push(card);
+    dealerhandSum += getValue(card);
+    dealerAce += getHandValue(dealerHand);
+  }
+  if (playerhandSum > 21) {
+    message = "Player Bust!, Dealer Wins!";
+    console.log("Player Bust!, Dealer Wins!");
+  } else if (playerhandSum > dealerhandSum || dealerhandSum > 21) {
+    message = "Player Wins!";
+    console.log("Player Wins!");
+  } else if (playerhandSum > 21 && dealerhandSum <= 21) {
+    message = "Dealer Wins!";
+    console.log("Dealer Wins!");
+  } else if (playerhandSum > 21 && dealerhandSum > 21) {
+    message = "No Winner!";
+    console.log("No Winner!");
+  } else if (playerhandSum < dealerhandSum) {
+    console.log("Dealer Wins!");
+    message = "Dealer Wins!";
+  } else if ((playerhandSum === dealerhandSum)) {
+    message = "House Rules, Dealer Wins!";
+  } else {
+    message = "Dealer Wins!";
+  }
+  document.getElementById("dealerhand").innerHTML = "Dealer -" + dealerhandSum;
+  document.getElementById("winmsg").innerText = message;
+  document.querySelector("playerhand").innerText = sumMsg;
+  return;
+}
+
+function showDealerCard() {
+  document.getElementById("dealercardimg").src =
+    "./blackjack-cards/" + hide + ".png";
+}
+
+function canHit() {
+  if (playerhandSum > 21) {
+    return;
+  }
+  let sumMsg = "";
+  const card = deck.pop();
+  playerHand.push(card);
+  playerhandSum += getValue(card);
+  playerAce += getHandValue(playerHand);
+  if (playerhandSum > 21) {
+    console.log("Player Bust!, Dealer Wins!");
+    showDealerCard();
+  } else {
+    console.log("Player drew a card: ", card);
+    console.log("Player hand sum:", playerhandSum);
+    console.log("Player ace count", playerAce);
+  }
+  let cardImg = document.createElement("img");
+  cardImg.src = "./blackjack-cards/" + card + ".png";
+  document.getElementById("player-card").append(cardImg);
+  document.getElementById("playerhand").innerText = "Player -" + playerhandSum;
+  document.getElementById("dealerhand").innerHTML = "Dealer -" + dealerhandSum;
 }
